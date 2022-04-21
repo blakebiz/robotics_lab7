65d2a635a51feaa71b5df915f38410eb2146bb0c
hsaeidi-uncw <saeidih@uncw.edu>
2022-04-21 13:34:28 -0400

    updated for real robot

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,5 @@ cd ~/catkin_ws/src
 git clone https://github.com/blakebiz/robotics_report2
 git clone https://github.com/blakebiz/robotics_lab4
 git clone https://github.com/blakebiz/robotics_lab6
-git clone https://github.com/hsaeidi-uncw/robot_vision_lectures
-git clone https://github.com/hsaeidi-uncw/ur5e_control
 cd ..
 catkin_make
